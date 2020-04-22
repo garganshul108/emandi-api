1e40d6d40bae72372528c1f80bc8a0079d24e56a
Anshul Garg <sci.agarg@gmail.com>
2020-04-22 19:34:21 +0530

    Testing MySQL Object

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const connection = require("./db/root");
 const app = express();
 const PORT = process.env.PORT || 8080;
+const config = require("config");
+
+const tf = require("./test_file");
+
+app.use("/tf", tf);
+
+connection.query("Select database() as db_name;", (err, result) => {
+  if (err) throw err;
+  console.log("Result: ", result[0].db_name);
+});
 
 app.get("/", (req, res) => {
   res.write("Emandi Server is active");
