401ef009ea40eb3ec0eed9055603a26586671fab
Anshul Garg <sci.agarg@gmail.com>
2020-04-27 22:19:40 +0530

    otp login-signup | not tested

--- routes/otp_login.js
+++ routes/otp_login.js
@@ -11,58 +11,15 @@ router.get("/", (req, res) => {
   if (!contact || !type) {
     return res.status(400).send('"type" and "contact" both/one not specified');
   }
+
+  const otp = otpGenerator();
+  let sql1, sql2;
   if (type === "vendor") {
-    return connectionPool.getConnection((err, connection) => {
-      if (err) {
-        console.log("Error in getting connection");
-        console.log(err);
-        return res.status(500).send("Internal Server Error");
-      }
-      let sql = `select vendor_id from VENDOR where contact=${contact};`;
-      return connection.query(sql, (err, results, fields) => {
-        if (err) {
-          console.log("Error while quering for id");
-          console.log(err);
-          connection.release();
-          return res.status(500).send("Error occured while quering");
-        }
-        if (results.length <= 0) {
-          connection.release();
-          return res.status(404).send("Unregistered Vendor");
-        }
-        vendor_id = results[0].vendor_id;
-        const otp = otpGenerator();
-        (async (req, res, connection) => {
-          try {
-            await sendOTP(contact, "otp-template-2", `${contact}|${otp}`);
-            let sql2 = `insert into OTP_LOGIN(subscriber_type, subscriber_id, contact, otp) values ("vendor",${vendor_id}, ${contact}, ${otp});`;
-            return connection.query(sql2, (err, results, fields) => {
-              if (err) {
-                console.log("Error while registering OTP");
-                console.log(err);
-                return res.status(500).send("Internal Server Error, OTP");
-              }
-              return res.status(201).send("OTP send and registered");
-            });
-          } catch (ex) {
-            return res.status(500).send("Error while sending the OTP");
-          }
-        })(req, res, connection);
-      });
-    });
+    sql1 = `select vendor_id from VENDOR where contact=${contact};`;
   } else if (type === "user") {
-  }
-
-  return res.status(400).send('"type" specified is not correct');
-});
-
-router.post("/", (req, res) => {
-  const { contact, otp, type } = req.body;
-  console.log("incoming", req.body);
-  if (!contact || !otp || !type) {
-    return res
-      .status(400)
-      .send('"type", "otp" and "contact" all or any one not specified');
+    sql1 = `select user_id from USER where contact=${contact};`;
+  } else {
+    return res.status(400).send('"type" specified is not correct');
   }
 
   return connectionPool.getConnection((err, connection) => {
@@ -71,43 +28,150 @@ router.post("/", (req, res) => {
       console.log(err);
       return res.status(500).send("Internal Server Error");
     }
-    let sql = `select * from OTP_LOGIN where contact=${contact}`;
-    return connection.query(sql, (err, results, fields) => {
+    return connection.query(sql1, (err, results, fields) => {
       if (err) {
-        console.log("Error occured while Quering");
+        console.log("Error while quering for id");
         console.log(err);
-        return res.status(500).send("Internal Server Error");
+        connection.release();
+        return res.status(500).send(err.message);
       }
       if (results.length <= 0) {
-        return res.status(404).send("OTP Expired");
-      }
-      const result = results[results.length - 1];
-      console.log("result", result);
-      if (otp === result.otp) {
-        let payload = {
-          contact: contact,
-          type: type,
-        };
-
-        if (type === "vendor") {
-          payload = {
-            ...payload,
-            vendor_id: result.subscriber_id,
-            isVendor: 1,
-          };
-        } else if (type === "user") {
-          payload = { ...payload, user_id: result.subscriber_id, isUser: 1 };
-        } else if (type === "admin") {
-          payload = { ...payload, admin_id: result.subscriber_id, isAdmin: 1 };
-        }
-
-        const token = jwt.generateToken(payload);
+        connection.release();
         return res
-          .header("x-auth-token", token)
-          .status(201)
-          .send("Login Successful!");
+          .status(404)
+          .send(`You are not registered as ${type} with ${contact}`);
       }
-      return res.status(404).send("Incorrect OTP");
+      if (type === "vendor") {
+        vendor_id = results[0].vendor_id;
+        sql2 = `insert into OTP_LOGIN(subscriber_type, subscriber_id, contact, otp) values ("vendor",${vendor_id}, ${contact}, ${otp});`;
+      } else if (type === "user") {
+        user_id = results[0].user_id;
+        sql2 = `insert into OTP_LOGIN(subscriber_type, subscriber_id, contact, otp) values ("user",${user_id}, ${contact}, ${otp});`;
+      }
+      (async (req, res, connection) => {
+        try {
+          await sendOTP(contact, "otp-template-2", `${contact}|${otp}`);
+          return connection.query(sql2, (err, results, fields) => {
+            if (err) {
+              console.log("Error while registering OTP");
+              console.log(err);
+              connection.release();
+              return res.status(500).send("Internal Server Error, OTP");
+            }
+            connection.release();
+            return res.status(201).send("OTP sent and registered");
+          });
+        } catch (ex) {
+          connection.release();
+          return res.status(500).send("Error while sending the OTP");
+        }
+      })(req, res, connection);
+    });
+  });
+});
+
+router.post("/", (req, res) => {
+  const { contact, otp, type, device_fcm_token } = req.body;
+  console.log("incoming", req.body);
+  if (!contact || !otp || !type || !device_fcm_token) {
+    return res
+      .status(400)
+      .send(
+        '"type", "otp", "contact", "device_fcm_token" all or any one not specified'
+      );
+  }
+
+  return connectionPool.getConnection((err, connection) => {
+    if (err) {
+      console.log("Error in getting connection");
+      console.log(err);
+      return res.status(500).send("Internal Server Error");
+    }
+    connection.beginTransaction((err) => {
+      if (err) {
+        console.log("Error while beginning transaction");
+        console.log(err);
+        connection.release();
+        return res.send(500).send("Internal Server Error");
+      }
+      let sql1 = `select * from OTP_LOGIN where contact=${contact}`;
+      return connection.query(sql1, (err, results, fields) => {
+        if (err) {
+          console.log("Error occured while Quering");
+          console.log(err);
+          connection.rollback(() => {
+            connection.release();
+          });
+          return res.status(500).send("Internal Server Error");
+        }
+        if (results.length <= 0) {
+          connection.rollback(() => {
+            connection.release();
+          });
+          return res.status(404).send("OTP expired/not registered");
+        }
+        const result = results[results.length - 1];
+        console.log("result", result);
+        if (otp === result.otp) {
+          let payload = {
+            contact: contact,
+            type: type,
+            device_fcm_token: device_fcm_token,
+          };
+
+          let sql2;
+          if (type === "vendor") {
+            payload = {
+              ...payload,
+              vendor_id: result.subscriber_id,
+              isVendor: 1,
+            };
+            sql2 = "select vendor_id from VENDOR where contact=${contact}";
+          } else if (type === "user") {
+            payload = { ...payload, user_id: result.subscriber_id, isUser: 1 };
+            sql2 = "select user_id from USER where contact=${contact}";
+          } else {
+            connection.rollback(() => {
+              connection.release();
+            });
+            return res.status(400).send('Invalid "type" specified');
+          }
+
+          connection.query(sql2, (err, results, fields) => {
+            if (err) {
+              connection.rollback(() => {
+                connection.release();
+              });
+              return res.status(500).send(err.message);
+            }
+            const result = results[0];
+            if (type === "vendor") {
+              let vendor_id = result.vendor_id;
+              payload = { ...payload, vendor_id };
+            } else if (type === "user") {
+              let user_id = result.user_id;
+              payload = { ...payload, user_id };
+            }
+
+            connection.commit((err) => {
+              if (err) {
+                console.log("Error while Commiting");
+                console.log(err);
+                res.status(500).send("Internal Server Error");
+              }
+
+              const token = jwt.generateToken(payload);
+              connection.release();
+              return res
+                .header("x-auth-token", token)
+                .status(201)
+                .send("Login Successful!");
+            });
+          });
+        }
+        connection.release();
+        return res.status(404).send("Incorrect OTP");
+      });
     });
   });
 });
--- routes/otp_signup.js
+++ routes/otp_signup.js
@@ -3,63 +3,76 @@ const router = express.Router();
 const connectionPool = require("../db/pool");
 const sendOTP = require("../util/otp_service");
 const jwt = require("../util/jwt");
+const otpGenerator = require("../util/otp_generator");
 
 router.get("/", (req, res) => {
   const { contact, type } = req.query;
   if (!contact || !type) {
-    return res.status(400).send('"type" and "contact" both/one not specified');
+    return res
+      .status(400)
+      .send('"type" and "contact" all/any one not specified');
   }
+  let sql1, sql2;
+  const otp = otpGenerator();
   if (type === "vendor") {
-    return connectionPool.getConnection((err, connection) => {
-      if (err) {
-        console.log("Error in getting connection");
-        console.log(err);
-        return res.status(500).send("Internal Server Error");
-      }
-      let sql = `select vendor_id from VENDOR where contact=${contact};`;
-      return connection.query(sql, (err, results, fields) => {
-        if (err) {
-          console.log("Error while quering for id");
-          console.log(err);
-          connection.release();
-          return res.status(500).send("Error occured while quering");
-        }
-        if (results.length > 0) {
-          connection.release();
-          return res.status(400).send("Vendor already registered!");
-        }
-
-        const otp = Math.floor(10000 + Math.random() * 90000);
-        (async (req, res, connection) => {
-          try {
-            await sendOTP(contact, "otp-template-2", `${contact}|${otp}`);
-            let sql2 = `insert into OTP_SIGNUP(subscriber_type, contact, otp) values ("vendor", ${contact}, ${otp});`;
-            return connection.query(sql2, (err, results, fields) => {
-              if (err) {
-                console.log("Error while registering OTP");
-                console.log(err);
-                return res.status(500).send("Internal Server Error, OTP");
-              }
-              return res.status(201).send("OTP send and registered");
-            });
-          } catch (ex) {
-            return res.status(500).send("Error while sending the OTP");
-          }
-        })(req, res, connection);
-      });
-    });
+    sql1 = `select vendor_id from VENDOR where contact=${contact};`;
+    sql2 = `insert into OTP_SIGNUP(subscriber_type, contact, otp) values ("vendor", ${contact}, ${otp});`;
   } else if (type === "user") {
+    sql1 = `select user_id from USER where contact=${contact};`;
+    sql2 = `insert into OTP_SIGNUP(subscriber_type, contact, otp) values ("user", ${contact}, ${otp});`;
+  } else {
+    return res.status(400).send('"type" specified is not correct');
   }
-  return res.status(400).send('"type" specified is not correct');
+  return connectionPool.getConnection((err, connection) => {
+    if (err) {
+      console.log("Error in getting connection");
+      console.log(err);
+      return res.status(500).send("Internal Server Error");
+    }
+
+    return connection.query(sql1, (err, results, fields) => {
+      if (err) {
+        console.log("Error while quering for id");
+        console.log(err);
+        connection.release();
+        return res.status(500).send("Error occured while quering");
+      }
+      if (results.length > 0) {
+        connection.release();
+        return res.status(400).send("Already registered!");
+      }
+
+      (async (req, res, connection) => {
+        try {
+          await sendOTP(contact, "otp-template-2", `${contact}|${otp}`);
+          return connection.query(sql2, (err, results, fields) => {
+            if (err) {
+              console.log("Error while registering OTP");
+              console.log(err);
+              connection.release();
+              return res.status(400).send(err.message);
+            }
+            connection.release();
+            return res.status(201).send("OTP send and registered");
+          });
+        } catch (ex) {
+          connection.release();
+          return res.status(500).send("Error while sending the OTP");
+        }
+      })(req, res, connection);
+    });
+  });
 });
 
 router.post("/", (req, res) => {
-  const { contact, otp, type } = req.body;
+  const { contact, otp, type, device_fcm_token } = req.body;
   console.log("incoming", req.body);
-  if (!contact || !otp || !type) {
+  if (!contact || !otp || !type || !device_fcm_token) {
     return res
       .status(400)
-      .send('"type", "otp" and "contact" all or any one not specified');
+      .send(
+        '"type", "otp", "contact", "device_fcm_token" maybe all or any one not specified'
+      );
   }
 
   return connectionPool.getConnection((err, connection) => {
@@ -68,43 +81,102 @@ router.post("/", (req, res) => {
       console.log(err);
       return res.status(500).send("Internal Server Error");
     }
-    let sql = `select * from OTP_LOGIN where contact=${contact}`;
-    return connection.query(sql, (err, results, fields) => {
+    return connection.beginTransaction((err) => {
       if (err) {
-        console.log("Error occured while Quering");
+        console.log("Error while beginning transaction");
         console.log(err);
+        connection.release();
         return res.status(500).send("Internal Server Error");
       }
-      if (results.length <= 0) {
-        return res.status(404).send("OTP Expired");
-      }
-      const result = results[results.length - 1];
-      console.log("result", result);
-      if (otp === result.otp) {
-        let payload = {
-          contact: contact,
-          type: type,
-        };
-
-        if (type === "vendor") {
-          payload = {
-            ...payload,
-            vendor_id: result.subscriber_id,
-            isVendor: 1,
-          };
-        } else if (type === "user") {
-          payload = { ...payload, user_id: result.subscriber_id, isUser: 1 };
-        } else if (type === "admin") {
-          payload = { ...payload, admin_id: result.subscriber_id, isAdmin: 1 };
+      let sql1 = `select * from OTP_SIGNUP where contact=${contact}`;
+      return connection.query(sql1, (err, results, fields) => {
+        if (err) {
+          console.log("Error occured while Quering");
+          console.log(err);
+          connection.rollback(() => {
+            connection.release();
+          });
+          return res.status(500).send("Internal Server Error");
         }
+        if (results.length <= 0) {
+          return res
+            .status(404)
+            .send("Not a valid OTP. OTP may be expired/not registered");
+        }
+        const result = results[results.length - 1];
+        console.log("result", result);
+        if (otp === result.otp) {
+          let payload = {
+            contact: contact,
+            type: type,
+            device_fcm_token: device_fcm_token,
+          };
 
-        const token = jwt.generateToken(payload);
-        return res
-          .header("x-auth-token", token)
-          .status(201)
-          .send("Login Successful!");
-      }
-      return res.status(404).send("Incorrect OTP");
+          let sql2, sql3;
+          if (type === "vendor") {
+            payload = {
+              ...payload,
+              vendor_id: result.subscriber_id,
+              isVendor: 1,
+            };
+            sql2 = `insert into VENDOR(contact) VALUES(${contact})`;
+            sql3 = "select LAST_INSERT_ID() as vendor_id from VENDOR";
+          } else if (type === "user") {
+            payload = {
+              ...payload,
+              vendor_id: result.subscriber_id,
+              isUser: 1,
+            };
+            sql2 = `insert into USER(contact) VALUES(${contact})`;
+            sql3 = "select LAST_INSERT_ID() as user_id from USER";
+          } else {
+            return res.status(400).send('"type" specified is not correct');
+          }
+
+          connection.query(sql2, (err, results, fields) => {
+            if (err) {
+              console.log(`Error while registering ${type} via OTP`);
+              console.log(err);
+              connection.rollback(() => {
+                connection.release();
+              });
+              return res.status(400).send(err.message);
+            }
+            connection.query(sql3, (err, results, fields) => {
+              if (err) {
+                console.log(`Error while retrieving ${type}_id`);
+                console.log(err);
+                connection.rollback(() => {
+                  connection.release();
+                });
+                return res.status(500).send("Internal Server Error");
+              }
+              const result = results[0];
+              if (type === "vendor") {
+                payload = { ...payload, vendor_id: result.vendor_id };
+              } else if (type === "user") {
+                payload = { ...payload, user_id: result.user_id };
+              }
+              const token = jwt.generateToken(payload);
+              return connection.commit((err) => {
+                if (err) {
+                  connection.rollback(() => {
+                    connection.release();
+                  });
+                  return res.status(500).send("Error while commiting changes");
+                }
+                connection.release();
+
+                return res
+                  .header("x-auth-token", token)
+                  .status(201)
+                  .send("Signup Successful!");
+              });
+            });
+          });
+        }
+        return res.status(404).send("Incorrect OTP");
+      });
     });
   });
 });
