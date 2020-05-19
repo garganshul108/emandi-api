5bc229d2159a872603d2c8e9c095234f6d8c25cf
Anshul Garg <sci.agarg@gmail.com>
2020-05-19 16:19:56 +0530

    To be modified

--- src/user/user.js
+++ src/user/user.js
@@ -1,66 +1,65 @@
-module.exports = function buildMakeUser({ valid, dateFormater, makeAddress }) {
+function buildMakeUser({isValid, makeAddress, makeDeviceFCMToken, makeTimestamp, makeProfilePicture, makeId, sanitize}){
   return function makeUser({
-    id,
-    device_fcm_token,
-    contact,
-    name,
-    address,
-    profile_picture,
-    reg_timestamp = Date.now(),
-  } = {}) {
-    if (!contact) {
-      throw new Error("User must have a contact number");
-    }
-    if (!device_fcm_token) {
-      throw new Error(
-        "User must have a device recognition string (device_fcm_token)"
-      );
-    }
-    if (
-      name &&
-      !valid(name, { type: "string", minLength: 2, onlyAlphabets: true })
-    ) {
-      throw new Error("User's name must be a valid, if provided");
-    }
-    if (state_id && !valid(state_id, { type: "number", minValue: 1 })) {
-      throw new Error("User's state_id must be a valid number, if provided");
-    }
-    if (city_id && !valid(city_id, { type: "number", minValue: 1 })) {
-      throw new Error("User's city_id must be a valid number, if provided");
-    }
-    if (pin_code && !valid(pin_code, { type: "number", minValue: 1 })) {
-      throw new Error("User's pin_code must be a valid number, if provided");
-    }
-    if (address && !valid(address, { type: "string", alphanumberic: true })) {
-      throw new Error("User's address should be a valid string");
-    }
+      id,
+      device_fcm_token,
+      contact,
+      name,
+      address,
+      profile_picture,
+      reg_timestamp
+  } = {}){
 
-    if (
-      profile_picture &&
-      !valid(profile_picture, { type: "string", specs: ["url"] })
-    ) {
-      throw new Error("User's profile picture is not a valid url");
-    }
+      if(name && !isValid(name, {type: "string", minLength:3})){
+          throw new Error("User's name is not valid");
+      }
 
-    reg_timestamp = dateFormater(reg_timestamp);
-    // filling up the user object
-    address = makeAddress(address);
-    return Object.freeze({
-      // utilities this object provide
-      // get functions
-      // edit functions
-      getName: () => name,
-      getId: () => {
-        if (!id) {
-          throw new Error("User's id is not assigned");
-        }
-        return id;
-      },
-      getContact: () => contact,
-      getAddress: () => address,
-      getOptions: () => {
-        profile_picture, device_fcm_token, reg_timestamp;
-      },
-    });
+      name = sanitize(name).trim();
+      if(name.length < 3){
+          throw new Error("User's name has no permissable characters");
+      }
+
+      
+      if(contact && !isValid(contact, {type:"number", exactDigitCount:10})){
+          throw new Error("User's contact is not valid");
+      }
+
+
+      const validId;
+      if(id){
+          validId = makeId(id);
+      }
+
+      const validDeviceFCMToken;
+      if(device_fcm_token){
+          validDeviceFCMToken = makeDeviceFCMToken(device_fcm_token);
+      }
+
+      const validProfilePicture;
+      if(profile_picture){
+          validProfilePicture= makeProfilePicture(profile_picture);
+      }
+
+      const validRegTimestamp;
+      if(reg_timestamp){
+          validRegTimestamp = makeTimestamp(reg_timestamp);
+      }
+
+      const validAddress;
+      if(address){
+          validAddress = makeAddress(address);
+      }
+
+      return Object.freeze({
+          getAddress:()=> validAddress,
+          getName:()=> name,
+          getContact:() => contact,
+          getProfilePicture:() => validProfile_picture,
+          getId:() => validId,
+          getDeviceFCMToken:()=> validDeviceFCMToken,
+          getRegTimestamp:() => validRegTimestamp
+      });
+
   };
-};
+}
+
+module.exports = buildMakeUser;
