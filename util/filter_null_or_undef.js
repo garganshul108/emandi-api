b5d596487075621600d8489fd018f6f80173ae6e
Anshul Garg <sci.agarg@gmail.com>
2020-05-20 23:49:47 +0530

    Added recursive check

--- util/filter_null_or_undef.js
+++ util/filter_null_or_undef.js
@@ -1,9 +1,14 @@
-module.exports = (object) => {
+const filter = (object) => {
   for (let key in object) {
     if (object[key] === undefined || object[key] === null) {
       delete object[key];
+    }
+    if (typeof object[key] === "object") {
+      object[key] = filter(object);
     }
   }
 
   return object;
 };
+
+module.exports = filter;
