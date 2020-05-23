6e5841240ccd4ccaccdcac2bc9667d676ebd6d0c
Anshul Garg <sci.agarg@gmail.com>
2020-05-23 15:58:57 +0530

    Tested Sequelize

--- v2/index.js
+++ v2/index.js
@@ -1,6 +1,27 @@
 const router = require("express").Router();
-const vendor = require("./src/vendor");
+// const vendor = require("./src/vendor");
 
-router.use("/vendor", vendor);
+// router.use("/vendor", vendor);
 
 module.exports = router;
+const db = require("./db");
+
+const arrayLyze = (results) =>
+  (results = Array.isArray(results)
+    ? results
+    : (results = results ? [results] : []));
+
+const extract = (result) => result.toJSON();
+
+// (async () => {
+//   try {
+//     let results = await db.VENDOR.findByPk(2, {
+//       include: [{ model: db.CITY, include: [{ model: db.STATE }] }],
+//     });
+//     results = arrayLyze(results);
+//     results = results.map((res) => extract(res));
+//     console.log(results);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// })();
