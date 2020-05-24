dc794e446367dfeb69586c918436110176686487
Anshul Garg <sci.agarg@gmail.com>
2020-05-24 12:22:23 +0530

    Added remove-city usecase

---
+++ v2/src/city/use-case/remove-city.js
@@ -0,0 +1,28 @@
+const makeCity = require("../city");
+
+module.exports = makeRemoveCity = ({ cityDb }) => {
+  const doesNotExist = () => {
+    return {
+      deletedCount: 0,
+      message: "City doesn't exists",
+    };
+  };
+  return (removeCity = async ({ id }) => {
+    if (!id) {
+      throw new Error("City id must be provided.");
+    }
+
+    const toBeDeleted = makeCity({ id });
+
+    const existing = cityDb.findById({ id: toBeDeleted.id });
+    if (!existing) {
+      return doesNotExist();
+    }
+
+    await cityDb.removeById({ id: toBeDeleted.getId() });
+    return {
+      deletedCount: 1,
+      message: "City deleted successfully",
+    };
+  });
+};
